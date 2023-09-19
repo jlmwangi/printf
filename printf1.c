81e59575772edc9a84b62bad1c99639995d42ad5
mwangi <jlmwangi254@gmail.com>
2023-09-20 01:53:14 +0300

    handle conversion specifiers

--- printf1.c
+++ printf1.c
@@ -11,19 +11,27 @@ int printf1_printf(const char *format, ...)
 	int n = 0;
 
 	va_start(arguments, format);
+	if (format == NULL)
+	{
+		return (-1);
+	}
 	while (*format)
 	{
+		if (*format == '%')
+		{
+			format++;
+		}
 		if (*format == 'd' || *format == 'i')
 		{
 			int d = va_arg(arguments, int);
 
 			write(1, &d, 4);
+			n++;
 		}
 		else
 		{
-			putchar(*format);
-			putchar('%');
-			n += 2;
+			write(1, format, 1);
+			n++;
 		}
 		format++;
 	}
