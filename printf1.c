e672b419a3e6da32148ae4e2eab8adc1365196cd
mwangi <jlmwangi254@gmail.com>
2023-09-20 01:07:51 +0300

    handle conversion specifiers

--- printf1.c
+++ printf1.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * printf1_printf - produce output according to a format
  * @format: pointer to a character constant
@@ -14,28 +13,19 @@ int printf1_printf(const char *format, ...)
 	va_start(arguments, format);
 	while (*format)
 	{
-		if (*format != '%')
+		if (*format == 'd' || *format == 'i')
 		{
-			putchar(*format);
-			n++;
+			int d = va_arg(arguments, int);
+
+			write(1, &d, 4);
 		}
 		else
 		{
-			format++;
-			if (*format == 'd' || *format == 'i')
-			{
-				int d = va_arg(arguments, int);
-
-				write(1, &d, 4);
-			}
-			else
-			{
-				putchar(*format);
-				putchar('%');
-				n += 2;
-			}
-			format++;
+			putchar(*format);
+			putchar('%');
+			n += 2;
 		}
+		format++;
 	}
 	va_end(arguments);
 	return (n);
